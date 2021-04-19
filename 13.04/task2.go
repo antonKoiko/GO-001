fcd90b76a80f91ae45aaaeb53456f535a0ab8d59
Anton Koiko <koiko.anton.dev@gmail.com>
2021-04-20 01:04:55 +0300

    Add task2 in 13.04

---
+++ 13.04/task2.go
@@ -0,0 +1,37 @@
+// Задача 2. Напишите функцию для подсчета частоты упоминания слов в строке текста и
+// возвращения карты со словами и числом, указывающем, сколько раз они употребляются.
+// Функция должна конвертировать текст в нижний регистр и обрезать знаки препинания.
+// Используйте пакет strings. Функции, которые пригодятся для выполнения данного задания: Fields, ToLower и Trim
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	text := "!Напишите функцию для подсчета частоты упоминания слов в строке текста и возвращения карты со словами и числом, указывающем, сколько раз они употребляются. Функция должна конвертировать текст в нижний регистр и обрезать знаки препинания. Используйте пакет strings. Функции, которые пригодятся для выполнения данного задания: Fields, ToLower и Trim"
+	fmt.Println("Частота упоминания слова в тексте: ")
+	words(text)
+}
+
+func words(text string) map[string]int {
+	text2 := strings.ToLower(text)
+	slice_text := strings.Fields(text2)
+	result := make(map[string]int)
+	for _, v := range slice_text {
+		value := strings.Trim(v, "!-,.")
+		_, ok := result[value]
+		if !ok {
+			result[value] = 1
+		}
+		if ok {
+			result[value] += 1
+		}
+	}
+	for k, v := range result {
+		fmt.Println(k, ":", v)
+	}
+	return result
+}
