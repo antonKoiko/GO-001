22600b3b0fadd809fba91c2558e65f0a03436cad
Anton Koiko <koiko.anton.dev@gmail.com>
2021-04-20 18:01:41 +0300

    Edit task2 in 13.04

--- 13.04/task2.go
+++ 13.04/task2.go
@@ -11,9 +11,12 @@ import (
 )
 
 func main() {
-	text := "!Напишите функцию для подсчета частоты упоминания слов в строке текста и возвращения карты со словами и числом, указывающем, сколько раз они употребляются. Функция должна конвертировать текст в нижний регистр и обрезать знаки препинания. Используйте пакет strings. Функции, которые пригодятся для выполнения данного задания: Fields, ToLower и Trim"
+	text := "!Напишите функцию для подсчета частоты упоминания слов в строке текста и возвращения карты со словами и числом, указывающем, сколько раз они употребляются. Функция должна конвертировать текст в нижний регистр и обрезать знаки препинания! Используйте пакет strings. Функции, которые пригодятся для выполнения данного задания: Fields, ToLower и Trim."
 	fmt.Println("Частота упоминания слова в тексте: ")
-	words(text)
+	result := words(text)
+	for k, v := range result {
+		fmt.Println(k, ":", v)
+	}
 }
 
 func words(text string) map[string]int {
@@ -22,16 +25,7 @@ func words(text string) map[string]int {
 	result := make(map[string]int)
 	for _, v := range slice_text {
 		value := strings.Trim(v, "!-,.")
-		_, ok := result[value]
-		if !ok {
-			result[value] = 1
-		}
-		if ok {
-			result[value] += 1
-		}
-	}
-	for k, v := range result {
-		fmt.Println(k, ":", v)
+		result[value]++
 	}
 	return result
 }
